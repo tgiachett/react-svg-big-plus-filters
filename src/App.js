5a6596054d90afd7e88a3e28ae32e1c96d72b4e5
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-04-27 10:09:12 -0500

    Optimized paths and adjusted Guassian blur

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Profile from './components/Profile'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Profile />
+      </div>
+    );
+  }
+}
+
+export default App;
